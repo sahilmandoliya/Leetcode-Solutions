90ad34ce2e9daf37285e00442cf209532a8436bf
Sahil Mandoliya <58983692+sahilmandoliya@users.noreply.github.com>
2022-07-10 10:10:54 +0530

    Time: 11 ms (91.29%), Space: 10.8 MB (46.85%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -4,8 +4,9 @@ public:
         unordered_map<int, int> unmap;
         for(int i=0;;i++){
             auto it=unmap.find(target-nums[i]);
-            if(it!=unmap.end())
+            if(it!=unmap.end()){
                 return vector<int> {i, it->second};
+            } 
             unmap[nums[i]]=i;
         }
     }
