0a37d54cfc145ad5add50480071da80a9400580c
Sahil Mandoliya <58983692+sahilmandoliya@users.noreply.github.com>
2022-08-27 07:12:28 +0530

    Time: 309 ms (93.51%), Space: 92.1 MB (78.68%) - LeetHub

---
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
+        int ans=INT_MIN;
+        priority_queue<pair<int, int>> p;
+        for(int i=0;i<points.size();i++){
+            while((!p.empty()) && (points[i][0]-p.top().second)>k) p.pop();
+            if(!p.empty()){
+                ans=max(ans, points[i][0]+points[i][1]+p.top().first);
+            }
+            p.push({points[i][1]-points[i][0], points[i][0]});
+        }
+        return ans;
+    }
+};
