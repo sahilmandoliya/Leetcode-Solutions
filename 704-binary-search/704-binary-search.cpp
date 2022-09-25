7b1b464d0e62ed72f5602a363f7c42ec3037747e
Sahil Mandoliya <58983692+sahilmandoliya@users.noreply.github.com>
2022-09-25 10:10:12 +0530

    Time: 54 ms (67.16%), Space: 27.6 MB (63.08%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -8,10 +8,10 @@ public:
             int mid=(left+right)/2;
             if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid]>target){
-                right=mid-1;
             }else if(nums[mid]<target){
-                left=mid+1;
+                left++;
+            }else{
+                right--;
             }
         }
         return -1;
