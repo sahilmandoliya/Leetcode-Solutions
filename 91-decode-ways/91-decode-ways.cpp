790b9db92d325e756b26a9b2bcf4c423e1734bf4
Sahil Mandoliya <58983692+sahilmandoliya@users.noreply.github.com>
2022-10-01 11:05:03 +0530

    Time: 3 ms (63.51%), Space: 6.2 MB (49.09%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -9,7 +9,7 @@ public:
             if(s[i]=='0') dp[i]=0;
             else{
                 dp[i]=dp[i+1];
-                if(i<n-1 && (s[i]=='1' || s[i]=='2' &&s[i+1]<'7')) dp[i]+=dp[i+2];
+                if(i<n-1 && (s[i]=='1' || s[i]=='2' && s[i+1]<'7')) dp[i]+=dp[i+2];
             }
         }
         return dp[0];
