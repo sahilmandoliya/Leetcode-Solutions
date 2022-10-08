37332176b20ac18afa3ff097e32016632ba1daba
Sahil Mandoliya <58983692+sahilmandoliya@users.noreply.github.com>
2022-10-08 23:47:52 +0530

    Time: 7 ms (20.50%), Space: 6 MB (47.54%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+int hammingWeight(uint32_t n) {
+    int ans=0;
+    for(int i=0;i<32;i++){
+        if(n&1<<i){
+            ans++;
+        }
+    }
+    return ans;
+}
+};
